4c55d48373d5170506286457ae24021d420aa283
Dylan Jackson <dylan.jackson@globaltranz.com>
2018-04-07 22:13:09 -0700

    first commit

---
+++ email_gen.py
@@ -0,0 +1,47 @@
+from selenium import webdriver 
+import sys
+
+import requests
+import bs4
+import re
+from selenium.webdriver.support.ui import WebDriverWait
+
+# Find Email 
+# match = emailRegex.findall(soup.text)
+
+def searchForEmails(string):
+
+    emailRegex = re.compile(r'[\w\.-]+@[\w\.-]+') 
+    match = emailRegex.findall(string)
+    print(match)
+    return(match)
+
+
+def GetEmail():
+    browser = webdriver.Firefox()
+    # browser.get("https://www.fakemailgenerator.com")
+
+    browser.get('https://www.fakemailgenerator.com')
+    browser.wait = WebDriverWait(browser, .5)
+
+    browser.execute_script("window.stop();")
+    browser.execute_script("window.stop();")
+
+    element = browser.find_element_by_css_selector('#cxtEmail')
+    print(element.text)
+    browser.close()
+
+count = int(input("How many emails would you like?:   "))
+for i in range(count):
+    GetEmail()
+
+# browser.close()
+
+
+
+
+
+
+
+
+
